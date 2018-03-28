b30c4de93378af59a1c69ee6a3b618bb5db2b74a
Roger Frank <rfrank@rfrank.net>
2018-03-28 16:48:12 -0600

    consolidated CSS into dpwb.css

--- test4.php
+++ test4.php
@@ -4,7 +4,7 @@
 	  <meta charset="utf-8">
 	  <meta name=viewport content="width=device-width, initial-scale=1">
     <title>DP Workbench</title>
-    <link rel="stylesheet" type="text/css" href="wb2.css">
+    <link rel="stylesheet" type="text/css" href="dpwb.css">
   </head>
 
   <body>
